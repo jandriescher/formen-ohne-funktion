92798225bb435724524be8cb255567aa8bbd862f
Jan Driescher <jandriescher@me.com>
2025-01-04 22:19:57 +0100

    adapt tile size

--- src/components/ContentPlane/ImagePlane/ImageGrid/index.js
+++ src/components/ContentPlane/ImagePlane/ImageGrid/index.js
@@ -2,12 +2,8 @@ import styled from 'styled-components'
 
 const ImageGridContainer = styled.div`
   display: grid;
-  min-height: 300px;
-  min-width: 300px;
-  max-height: 500px;
-  max-width: 500px;
-  height: 100%;
-  width: 100%;
+  width: 900px;
+  height: 900px;
   overflow: auto;
   grid-template-columns: [first] 50% [line2] 50% [end];
   grid-template-rows: [row1-start] 50% [row2-start] 50% [last-line];
